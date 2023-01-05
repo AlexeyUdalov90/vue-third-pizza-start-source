9a7ef53601f8c954cbe38e5b581c60f1461de6ac
Aleksey Udalov <armiman69@gmail.com>
2023-01-05 15:05:37 +0300

    Добавил шаблоны для шапки и главной страницы

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <app-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from '@/layouts/AppHeader.vue'
+// export default {
+//   name: 'AppLayout',
+//   components: {
+//     AppHeader
+//   }
+// }
+</script>
+
+<style scoped>
+
+</style>
